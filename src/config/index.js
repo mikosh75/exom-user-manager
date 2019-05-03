92786817b415cfe8cafb4cc8edda5ac5c0d19456
ms <mikosh@gmail.com>
2019-05-04 01:40:37 +0200

    url to update user

--- src/config/index.js
+++ src/config/index.js
@@ -3,12 +3,11 @@ export default {
   clientid: "a32a5f35-c2ce-4a79-8f9c-fed21bae7f49",
   redirecturl: "http://localhost:8080",
   // authority: 'https://login.microsoftonline.com/adf2a433-a1e8-459f-8692-a8941c5be110', // msnetpro
-  authority:
-    "https://login.microsoftonline.com/c261ec17-d65f-4219-ad99-d4fa5ceb2ec2", // exom directory
+  authority: "https://login.microsoftonline.com/c261ec17-d65f-4219-ad99-d4fa5ceb2ec2", // exom directory
   graphscopes: ["user.read"],
   graphendpoint: "https://graph.microsoft.com/v1.0/me",
   graphendpointinvitation: "https://graph.microsoft.com/v1.0/invitations",
-  graphendpointaddusertogroup:
-    "https://graph.microsoft.com/v1.0/groups/044532dc-a224-4744-8e68-70dc06d29811/members/$ref",
+  graphendpointupdateuser: "https://graph.microsoft.com/v1.0/users",
+  graphendpointaddusertogroup: "https://graph.microsoft.com/v1.0/groups/044532dc-a224-4744-8e68-70dc06d29811/members/$ref",
   appinsightsid: ""
 };
