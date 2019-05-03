3e950ee5410511a0ae8212a900a262518c11c72a
ms <mikosh@gmail.com>
2019-05-04 01:38:03 +0200

    breadcrumbs component

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-breadcrumbs :items="items" divider=">"></v-breadcrumbs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Breadcrumbs",
+  data: function() {
+    return {};
+  },
+  props: {
+    value: { type: Boolean, default: false },
+    message: { type: String, default: "Please wait..." },
+    progressColor: { type: String, default: "amber" }
+  }
+};
+</script>
+
+<style>
+</style>
